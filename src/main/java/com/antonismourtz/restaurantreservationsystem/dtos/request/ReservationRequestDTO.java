8f6fab5f3c684a3b03a084bfc5bb8482d8c28a24
Antonis Mourtz <mourtzakisa@gmail.com>
2025-06-11 22:36:21 +0300

    Implement system logic for automatic table assignment and reservation

--- src/main/java/com/antonismourtz/restaurantreservationsystem/dtos/request/ReservationRequestDTO.java
+++ src/main/java/com/antonismourtz/restaurantreservationsystem/dtos/request/ReservationRequestDTO.java
@@ -3,7 +3,6 @@ package com.antonismourtz.restaurantreservationsystem.dtos.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
